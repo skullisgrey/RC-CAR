2005fdfd297c46e36354ca349149999508c4a5fd
skullisgrey <gandhiforce@gmail.com>
2025-11-19 12:16:03 +0900

    Create uart3_printf.c

---
+++ uart3_printf.c
@@ -0,0 +1,27 @@
+#include "uart3_printf.h"
+
+extern UART_HandleTypeDef huart3;
+
+#ifdef __GNUC__
+/* With GCC, small printf (option LD Linker->Libraries->Small printf
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART1 and Loop until the end of transmission */
+  if (ch == '\n')
+    HAL_UART_Transmit (&huart3, (uint8_t*) "\r", 1, 0xFFFF);
+  HAL_UART_Transmit (&huart3, (uint8_t*) &ch, 1, 0xFFFF);
+
+  return ch;
+}
